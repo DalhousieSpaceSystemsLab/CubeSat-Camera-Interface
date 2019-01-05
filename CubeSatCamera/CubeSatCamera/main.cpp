0c4ed2281a47323d5538f34f4785198ef2c10f82
Liam Hebert <liam.hebert@dal.ca>
2019-01-04 23:47:04 -0400

    CommandLine args + a little cleanup
    Added the following args:
    Command | Purpose | Default
    ------------ | ------------- | -------------
    -q | Runs the program without creating logs |
    -nc0 | Ignores camera 0 |
    -nc1 | Ignores camera 1 |
    -p * | Sets file path to * | Pictures/*date*
    -n * | Sets file name to * | *date*
    -c * | Sets file format to * | jpeg
    
    also added a bunch of comments and cleaned up the code a little.

--- CubeSatCamera/CubeSatCamera/main.cpp
+++ CubeSatCamera/CubeSatCamera/main.cpp
@@ -16,23 +16,38 @@ using namespace cv;
 
 static int errorCount;
 static long startTime;
+static bool quiet;
 static ofstream printer;
-//takes a picture
-//NOTE: picture currently saves in
+
+//gets today's date in YYYY-MM-DD format
+string getDate() {
+    time_t now = time(nullptr);
+    tm *ltm = localtime(&now);
+    string year = to_string(1900 + ltm -> tm_year);
+    string month = to_string(1 + ltm -> tm_mon);
+    string day = to_string(ltm -> tm_mday);
+    return year + "-" + month + "-" + day;
+}
+
+//gets local time in HH:MM:SS format
+string getTime() {
+    time_t now = time(nullptr);
+    tm *ltm = localtime(&now);
+    long ms = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
+    return to_string(ltm -> tm_hour) + ":" + to_string(ltm -> tm_min) + ":" + to_string(ltm -> tm_sec) + " (" + to_string(ms) + ")";
+}
+
+//grabs next frame from camera
 Mat capture(VideoCapture cap) {
     Mat frame;
     cap.grab(); // grabs next frame
     cap.read(frame); // saves picture to frame
-
-    // saves picture as .jpeg (includes compression)
-    //bool success = imwrite("Pictures/" + name + ".jpeg", frame);
-    //imwrite(name + ".png", frame);
-
     return frame;
 }
 
-bool compress(const Mat &frame, const String &name) {
-    return imwrite("Pictures/" + name + ".jpeg", frame);
+//takes frame and saves it at path with specified compression format
+bool compress(const Mat &frame, const string &path, const string &name, const string &compression) {
+    return imwrite(path + name + "." + compression, frame);
 }
 
 //calibrates camera appropriately
@@ -42,53 +57,50 @@ bool calibrate() {
     return false;
 }
 
-void logError(int level, const String &msg) {
-    //gathering sys time
-    time_t now = time(nullptr);
-    tm *ltm = localtime(&now);
-
-    long ms = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
-
-    string time = to_string(ltm -> tm_hour) + ":" + to_string(1 + ltm -> tm_min) + ":" + to_string(1 + ltm -> tm_sec) + " (" + to_string(ms) + ")";
-
-
-    if (!printer.is_open()) {
-        cerr << "[" << time << "] " << "ErrorLogs Failed to open, no errors today!";
-    }
-    else {
-        if (level == -1) { //new line
-            printer << endl;
-            return;
+//logs errors and actions into Error Logs/(date).txt
+void log(int level, const string &msg) {
+    if (!quiet) { //if quiet, no logs are created
+        if (!printer.is_open()) {
+            cerr << "[" << getTime() << "] " << "ErrorLog Failed to open, no errors today!" << endl;
         }
 
-        printer << "[" << time << "] ";
-        switch(level) { //error tags
-            case (1) :
-                printer << "[SEVERE] "; //program breaking errors
-                errorCount++;
-                break;
-            case (2) :
-                printer << "[MODERATE] "; //things to look into
-                errorCount++;
-                break;
-            case (99) :
-                printer << "[ACTION] "; //logging important actions
-                break;
-            case (999) : //no tag
-                break;
-            default :
-                printer << "[LEVEL " << level << "] "; //custom level errors
-                errorCount++;
-                break;
+        else {
+            if (level == -1) { //new line
+                printer << endl;
+                return;
+            }
+
+            printer << "[" << getTime() << "] ";
+            switch (level) { //error tags
+                case (1) :
+                    printer << "[SEVERE] "; //program breaking errors
+                    errorCount++;
+                    break;
+                case (2) :
+                    printer << "[MODERATE] "; //things to look into
+                    errorCount++;
+                    break;
+                case (99) :
+                    printer << "[ACTION] "; //logging important actions
+                    break;
+                case (999) : //no tag
+                    break;
+                default :
+                    printer << "[LEVEL " << level << "] "; //custom level errors
+                    errorCount++;
+                    break;
+            }
+            printer << msg << endl;
         }
-        printer << msg << endl;
+        printer.flush(); //prints message to file
     }
-    printer.flush();
 }
 
 int main(int argc, const char * argv[]) {
     startTime = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
     errorCount = 0;
+
+    //TODO - check if the dir exists, might reduce time
     int dirResult = mkdir("Error Logs", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); //creating directory with basic read/write permissions
     if (dirResult != -1) {
         cerr << "Error Creating Error Logs Folder";
@@ -102,99 +114,160 @@ int main(int argc, const char * argv[]) {
         //exit(1); //might be an idea to mitigate around this
     }
 
-    //gathering sys time
-    time_t now = time(nullptr);
-    tm *ltm = localtime(&now);
-    string year = to_string(1900 + ltm -> tm_year);
-    string month = to_string(1 + ltm -> tm_mon);
-    string day = to_string(ltm -> tm_mday);
+    //parameters
+    quiet = false;
+    bool useCam0 = true;
+    bool useCam1 = true;
+    bool defaultPath = true;
+    string filePath;
+    bool defaultName = true;
+    string fileName;
+    bool defaultCompression = true;
+    string compression;
 
-    printer.open("Error Logs/" + year + "-" + month + "-" + day + ".txt", ios_base::app); //opening file or creating new one if does not exist
+    //gathering commandline args
+    string arg;
+    for (int i = 1; i < argc; i++) {
+        arg = argv[i];
+        if (arg == "-q") //no logs
+            quiet = true;
+        else if (arg == "-nc0") //ignore camera 0
+            useCam0 = false;
+        else if (arg == "-nc1") //ignore camera 1
+            useCam1 = false;
+        //TODO - clean this up
+        else if (arg == "-p") { //custom path
+            if (i + 1 < argc) { //room for filename
+                arg = argv[i + 1];
+                if (arg.at(0) != '-') { //valid format
+                    filePath = arg;
+                    defaultPath = false;
+                    i++;
+                }
+                else {
+                    arg = argv[i];
+                    cerr << "invalid path format";
+                }
+            } else cerr << "missing path string";
+        }
+        else if (arg == "-n") { //custom filename
+            if (i + 1 < argc) {
+                arg = argv[i + 1];
+                if (arg.at(0) != '-') {
+                    fileName = arg;
+                    defaultName = false;
+                    i++;
+                }
+                else {
+                    arg = argv[i];
+                    cerr << "invalid file name format";
+                }
+            } else cerr << "missing file name string";
+        }
+        else if (arg == "-c") { //custom compression
+            if (i + 1 < argc) {
+                arg = argv[i + 1];
+                if (arg.at(0) != '-') {
+                    compression = arg;
+                    defaultCompression = false;
+                    i++;
+                }
+                else {
+                    arg = argv[i];
+                    cerr << "invalid compression format";
+                }
+            } else cerr << "missing compression string";
+        }
+    }
 
-
-    logError(999, "START OF RUNTIME");
-    // TODO - Create config.txt (or fancy, config.xml)
-    // TODO - read from file for path name, camera ID and maybe other things if needed
-
-
-    //--- INITIALIZE VIDEOCAPTURE
+    //gathering sys date
+    string date = getDate();
+    if (!quiet) { //create logs if not in quiet mode
+        printer.open("Error Logs/" + date + ".txt", ios_base::app); //opening file or creating new one if does not exist
+        log(999, "START OF RUNTIME"); //I know what you are thinking, I lied here. shhhh
+    }
+    if (defaultPath) //no custom path
+        filePath = "Pictures/";
+    if (defaultName) //no custom file name
+        fileName = date;
+    if (defaultCompression) //no custom compression method
+        compression = "jpeg";
 
     // initializing using default API
-    //logError(99, "Initializing cameras...");
+    log(99, "Initializing cameras...");
 
     // camera 0
     VideoCapture capture0;
-    capture0.open(0);
+    if (useCam0) {
+        capture0.open(0);
+    }
 
     // camera 1
     VideoCapture capture1;
-    capture1.open(1);
-
-
-    // OR advance usage: select any API backend
-    //int deviceID = 0;             // 0 = open default camera
-    //int apiID = CAP_ANY;      // 0 = autodetect default API
-    // open selected camera using selected API
-    //cap.open(deviceID + apiID);
+    if (useCam1) {
+        capture1.open(1);
+    }
 
 
     //take picture and test for success
-    //TODO - Figure out better naming system for two photos
 
-    logError(99, "Taking pictures...");
+    log(99, "Taking pictures...");
 
     Mat picture0;
     Mat picture1;
-    Mat *p0 = &picture0;
-    Mat *p1 = &picture1;
 
     //TODO - Potentially implement multithreading here :)
     if (capture0.isOpened()) {
         picture0 = capture(capture0);
-        logError(99, "Camera 0 frame grab");
-
-    } else logError(2, "Camera 0 failed to open");
+        log(99, "Camera 0 frame grab");
+    }
+    else if (useCam0)
+        log(2, "Camera 0 failed to open");
 
     if (capture1.isOpened()) {
         picture1 = capture(capture1);
-        logError(99, "Camera 1 frame grab");
+        log(99, "Camera 1 frame grab");
 
-    } else logError(2, "Camera 1 failed to open");
+    }
+    else if (useCam1)
+        log(2, "Camera 1 failed to open");
 
     long endTime = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
-    logError(999, "Picture Taking Time: " + to_string(endTime - startTime) + "ms");
+    log(999, "Picture Taking Time: " + to_string(endTime - startTime) + "ms");
 
-//    capture0.release();
-//    logError(99, "Camera0 released");
-//    capture1.release();
-//    logError(99, "Camera1 released");
 
-    logError(99, "Compressing...");
+    log(99, "Compressing...");
 
     //checking if pictures saved, if saved, compress
     //camera 0
-    if (p0 != nullptr) {
-        if (compress(picture0, "name" "(C0)"))
-            logError(99, "Camera 0 Success! Saved at name (C0)");
+    if (capture0.isOpened()) {
+        if (compress(picture0, filePath, fileName + " (C0)", compression))
+            log(99, "Camera 0 Success! Saved at " + filePath + fileName + " (C1)" + "." + compression);
         else
-            logError(1, "Camera 0 failed but opened, possible defect or bug");
+            log(1, "Camera 0 failed but opened, possible defect or bug");
     }
 
     //camera 1
-    if (p1 != nullptr) {
-        if (compress(picture1, "name" "(C1)"))
-            logError(99, "Camera 1 Success! Saved at name (C1)");
+    if (capture1.isOpened()) {
+        if (compress(picture1, filePath, fileName + " (C1)", compression))
+            log(99, "Camera 1 Success! Saved at " + filePath + fileName + " (C1)" + "." + compression);
         else
-            logError(1, "Camera 1 failed but opened, possible defect or bug");
-    } 
+            log(1, "Camera 1 failed but opened, possible defect or bug");
+    }
 
 
-    logError(999, "END OF RUNTIME");
+    log(999, "END OF RUNTIME");
 
     endTime = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
-    logError(999, "Runtime: " + to_string(endTime - startTime) + "ms - Error Count: " + to_string(errorCount));
-    logError(-1, "");
-    //destroying objects
+
+    long runTime = endTime - startTime;
+    if (quiet)
+        cout << "Runtime: " << to_string(runTime) << "ms";
+    else {
+        log(999, "Runtime: " + to_string(runTime) + "ms - Error Count: " + to_string(errorCount));
+        log(-1, "");
+    }
+    //destroying
     capture0.release();
     capture1.release();
     printer.close();
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# Camera Software Outline (v1)
+# CubeSat Camera
 ## Overview: 
 This software will function as the interface between the camera modules (Color and NIR) and the operating system. The software will utilize the OpenCV library to capture photos from the two different cameras when run then save them in a specified area. We will also take into account various problems that may arise in a spatial environment by considering multiple countermeasures.  
 
@@ -19,3 +19,16 @@ This software will function as the interface between the camera modules (Color a
 - Library of various Computer Vision interfaces and algorithms. 
 - Specifically, imgproc, imgcodecs, calib3d and VideoCapture modules will be used
 - *Dependencies may increase as development progresses*
+
+### Command Line Args:
+The following arguments are supported
+
+Command | Purpose | Default
+------------ | ------------- | -------------
+-q | Runs the program without creating logs | 
+-nc0 | Ignores camera 0 |
+-nc1 | Ignores camera 1 |
+-p * | Sets file path to * | Pictures/*date*
+-n * | Sets file name to * | *date*
+-c * | Sets file format to * | jpeg
+
